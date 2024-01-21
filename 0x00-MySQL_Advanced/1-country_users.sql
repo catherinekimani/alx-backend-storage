56fb372d2d45c167c898435a61828ff22f675d08
catherinekimani <catherinekimani882@gmail.com>
2024-01-21 22:25:56 +0300

    Script to create a table

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+--  SQL script that creates a table users
+CREATE TABLE IF NOT EXISTS (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
