67dacef667fc0a1897d15c6a647b8038521496d3
catherinekimani <catherinekimani882@gmail.com>
2024-01-23 00:48:34 +0300

    script that creates a view need_meeting

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,5 @@
+-- script that creates a view need_meeting
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE (score < 80 AND (students.last_meeting IS NOT NULL OR students.last_meeting < CURDATE() - INTERVAL 1 MONTH));
