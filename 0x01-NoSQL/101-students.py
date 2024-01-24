dd386f8ccf6e2160921b9409b3d69b8fe9070604
catherinekimani <catherinekimani882@gmail.com>
2024-01-25 01:01:26 +0300

    Add test files to gitignore

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -7,6 +7,6 @@ def top_students(mongo_collection):
     return all students sorted by average score
     """
     return mongo_collection.aggregate([
-        {"$project": {"name": 1, "averageScore": {"$avg": "topics.score"}}},
+        {"$project": {"name": 1, "averageScore": {"$avg": "$topics.score"}}},
         {"$sort": {"averageScore": -1}}
     ])
