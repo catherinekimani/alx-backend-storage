3e77c99caeef5b2d5b5d93878e13cdc32cf4b72f
catherinekimani <catherinekimani882@gmail.com>
2024-01-25 00:32:41 +0300

    function that returns all students sorted by average score

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" function that returns all students sorted by average score """
+
+
+def top_students(mongo_collection):
+    """
+    return all students sorted by average score
+    """
+    return mongo_collection.aggregate([
+        {"$project": {"name": 1, "averageScore": {"$avg": "topics.score"}}},
+        {"$sort": {"averageScore": -1}}
+    ])
